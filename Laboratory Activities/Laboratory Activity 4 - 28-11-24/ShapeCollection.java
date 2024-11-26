d197c4f111cd56ceee17e8f2a12bdceb8926cf5f
andavael <162865770+andavael@users.noreply.github.com>
2024-11-26 19:23:46 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 4 - 28-11-24/ShapeCollection.java
@@ -0,0 +1,16 @@
+public class ShapeCollection {
+    private Shape[] shapes;
+    
+    public ShapeCollection(Shape[] shapes) {
+        this.shapes = shapes;
+    }
+    
+    public void getPropertyValues() {
+        for (Shape shape : shapes) {
+            shape.printShapeType();
+            System.out.println(String.format("Area: %.2f", shape.getArea()));
+            System.out.println(String.format("Perimeter: %.2f", shape.getPerimeter()));
+            System.out.println();
+        }
+    }
+}
